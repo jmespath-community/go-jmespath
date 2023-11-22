aa8d53ecec1e77574807ed8b13c5d3a35326e897
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-11-22 21:31:33 +0100

    feat: use cobra for the CLI (#83)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/jpgo/main.go
+++ cmd/jpgo/main.go
@@ -18,78 +18,80 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 
 	"github.com/jmespath-community/go-jmespath/pkg/api"
 	"github.com/jmespath-community/go-jmespath/pkg/parsing"
+	"github.com/spf13/cobra"
 )
 
-func errMsg(msg string, a ...interface{}) int {
-	fmt.Fprintf(os.Stderr, msg, a...)
-	fmt.Fprintln(os.Stderr)
-	return 1
+func main() {
+	var command command
+	cmd := &cobra.Command{
+		Use:  "jpgo",
+		Args: cobra.ExactArgs(1),
+		RunE: command.run,
+	}
+	cmd.Flags().BoolVar(&command.astOnly, "ast", false, "Print the AST for the input expression and exit.")
+	cmd.Flags().StringVar(&command.inputFile, "input", "", "Filename containing JSON data to search. If not provided, data is read from stdin.")
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
-func run() int {
-	astOnly := flag.Bool("ast", false, "Print the AST for the input expression and exit.")
-	inputFile := flag.String("input", "", "Filename containing JSON data to search. If not provided, data is read from stdin.")
+type command struct {
+	astOnly   bool
+	inputFile string
+}
 
-	flag.Parse()
-	args := flag.Args()
+func (c command) run(cmd *cobra.Command, args []string) error {
 	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "Usage:\n\n")
-		flag.PrintDefaults()
-		return errMsg("\nError: expected a single argument (the JMESPath expression).")
+		return errors.New("error: expected a single argument (the JMESPath expression)")
 	}
-
 	expression := args[0]
 	parser := parsing.NewParser()
 	parsed, err := parser.Parse(expression)
 	if err != nil {
 		if syntaxError, ok := err.(parsing.SyntaxError); ok {
-			return errMsg("%s\n%s\n", syntaxError, syntaxError.HighlightLocation())
+			return fmt.Errorf("%s\n%s", syntaxError, syntaxError.HighlightLocation())
 		}
-		return errMsg("%s", err)
+		return err
 	}
-	if *astOnly {
+	if c.astOnly {
 		fmt.Println("")
 		fmt.Printf("%s\n", parsed)
-		return 0
+		return nil
 	}
-
 	var inputData []byte
-	if *inputFile != "" {
-		inputData, err = os.ReadFile(*inputFile)
+	if c.inputFile != "" {
+		inputData, err = os.ReadFile(c.inputFile)
 		if err != nil {
-			return errMsg("Error loading file %s: %s", *inputFile, err)
+			return fmt.Errorf("error loading file %s: %w", c.inputFile, err)
 		}
 	} else {
 		// If an input data file is not provided then we read the
 		// data from stdin.
 		inputData, err = io.ReadAll(os.Stdin)
 		if err != nil {
-			return errMsg("Error reading from stdin: %s", err)
+			return fmt.Errorf("error reading from stdin: %w", err)
 		}
 	}
 	var data interface{}
 	if err := json.Unmarshal(inputData, &data); err != nil {
-		return errMsg("Invalid input JSON: %s", err)
+		return fmt.Errorf("invalid input JSON: %w", err)
 	}
 	result, err := api.Search(expression, data)
 	if err != nil {
-		return errMsg("Error executing expression: %s", err)
+		return fmt.Errorf("error executing expression: %w", err)
 	}
 	toJSON, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
-		return errMsg("Error serializing result to JSON: %s", err)
+		return fmt.Errorf("error serializing result to JSON: %w", err)
 	}
 	fmt.Println(string(toJSON))
-	return 0
-}
-
-func main() {
-	os.Exit(run())
+	return nil
 }
