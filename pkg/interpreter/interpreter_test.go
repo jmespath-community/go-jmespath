22c8032bd7267cc0de927761f9331f0e40d2f0a7
Springcomp <springcomp@users.noreply.github.com>
2025-01-14 12:29:42 +0000

    Added unit-test for failing case
    Signed-off-by: Springcomp <springcomp@users.noreply.github.com>

--- pkg/interpreter/interpreter_test.go
+++ pkg/interpreter/interpreter_test.go
@@ -200,6 +200,13 @@ func TestCanSupportEvaluatingRightHandSideOfStringSlice(t *testing.T) {
 	assert.Nil(err)
 	assert.Equal(result.(float64), 3.0)
 }
+func TestErrEvaluatingRightHandSideOfStringSlice(t *testing.T) {
+	assert := assert.New(t)
+	data := make(map[string]interface{})
+	_, err := search(t, "'foo'[:].unknown(@)", data)
+	assert.NotNil(err)
+	assert.Equal("unknown function: unknown", err.Error())
+}
 
 func BenchmarkInterpretSingleFieldStruct(b *testing.B) {
 	assert := assert.New(b)
